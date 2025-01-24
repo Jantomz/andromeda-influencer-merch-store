2d9f7d12a88c5c530e8c96bd9143161ab14d8504
Jantomz <zjaden28@gmail.com>
2025-01-24 14:49:27 -0500

    Changed responsivity

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,13 @@ const Page = async (props: Props) => {
         <main>
             <div className="h-24"></div>
             <header className="flex gap-2 flex-col justify-center items-center z-10 relative">
-                <h1 className="z-30 text-7xl max-md:text-5xl font-semibold w-full max-md:w-3/4 text-center text-white mt-36">
+                <h1 className="z-30 text-7xl max-md:text-3xl font-semibold w-full max-md:w-full text-center text-white mt-36">
                     Your Decentralized Ticketing
                 </h1>
-                <h2 className="z-40 text-3xl max-md:text-2xl font-semibold w-1/2 max-md:w-3/4 text-center text-white">
+                <h2 className="z-40 text-3xl max-md:text-xl font-semibold w-1/2 max-md:w-3/4 text-center text-white">
                     Ticket3
                 </h2>
-                <h2 className="z-40 text-sm max-md:text-2xl font-base w-1/2 max-md:w-3/4 text-gray-500 text-center hover:text-gray-400">
+                <h2 className="z-40 text-sm max-md:text-xl font-base w-1/2 max-md:w-3/4 text-gray-500 text-center hover:text-gray-400">
                     <a
                         href="https://github.com/Jantomz/andromeda-ticket-3"
                         target="_blank"
